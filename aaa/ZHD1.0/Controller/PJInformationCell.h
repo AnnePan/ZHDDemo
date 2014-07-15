02df6fff08f81fefd15c97e7405ec33ed47deaae
panjuanjuan <panjuanjuan@pajk.cn>
2014-07-15 18:42:57 +0800

    最新资讯开始写

---
+++ aaa/ZHD1.0/Controller/PJInformationCell.h
@@ -0,0 +1,13 @@
+//
+//  PJInformationLatestCell.h
+//  ZHD1.0
+//
+//  Created by hamigua on 14-7-15.
+//  Copyright (c) 2014年 com.pjj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PJInformationLatestCell : UITableViewCell
+
+@end
---
+++ aaa/ZHD1.0/Controller/PJInformationCell.m
@@ -0,0 +1,34 @@
+//
+//  PJInformationLatestCell.m
+//  ZHD1.0
+//
+//  Created by hamigua on 14-7-15.
+//  Copyright (c) 2014年 com.pjj. All rights reserved.
+//
+
+#import "PJInformationLatestCell.h"
+
+@implementation PJInformationLatestCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
